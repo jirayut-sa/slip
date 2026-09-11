7130e850b8ed1730812a9b3e28ac992d79b29b24
jirayut-sa <122371918+jirayut-sa@users.noreply.github.com>
2026-09-11 22:32:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+/* Public endpoint only. Never put LINE/Gemini secrets here. */
+window.SEC1_CONFIG={"apiUrl": "https://script.google.com/macros/s/AKfycbyx6FHcm9gs3TeEopijpRug8z8sXpo9iLm6v3qavCj0-2W8zwA6keSWgAc6qiPj7mVzXw/exec", "liffId": "2011470307-gvcrsOhk"};
